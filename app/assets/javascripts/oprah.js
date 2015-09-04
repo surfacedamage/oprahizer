4d29c8364b84902fa33134d630f8c9546aa19858
Aaron Christy <aaron@phad.com>
2015-09-04 14:45:38 -0400

    just use html placeholder

--- app/assets/javascripts/oprah.js
+++ app/assets/javascripts/oprah.js
@@ -2,13 +2,4 @@ $(document).ready(function(){
 
   $('#generator').find('input[type=text]').focus();
 
-  $('#generator').on('submit', function(e){
-    $('#oprahized').fadeOut('slow');
-    var form  = $(this);
-    var say   = form.find(form.find('input[type=text]'));
-    form.attr('action', say.val());
-  });
-
-  $('label').inFieldLabels();
-
 });
