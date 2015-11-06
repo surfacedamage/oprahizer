4fc36d490772a4c0ac644fa607ed6a21e5b89205
Aaron Christy <aaron@phad.com>
2015-11-06 10:30:30 -0500

    add route directly for image processing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Oprahize::Application.routes.draw do
   match 'win'   => 'animations#index'
   match '/:say' => 'animations#index'
+  match '/images/:say'=> 'animations#index'
   root :to      => 'animations#index'
 end
